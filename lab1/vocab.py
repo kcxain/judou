a449e1da8bf8197e5cca958efe221bcd6e0b9f5b
Deconx <deconx@vip.qq.com>
2022-10-17 18:40:34 +0800

    add some regex

--- lab1/vocab.py
+++ lab1/vocab.py
@@ -6,29 +6,51 @@ class Vocab:
     def __init__(self, data_file='../data/199801_seg&pos.txt', target_file='../data/dict.txt'):
         self.data_file = data_file
         self.target_file = target_file
-        # 数字串统一为<numbers>
-        self.pad = '<numbers>'
+        # 数字串统一为<n>
+        self.pad = '<n>'
         # 保存词典
         self.d = dict()
         # 保存正则表达式匹配数字后面可能有的字符
         self.s = set()
+        # 保存全部正则匹配规则
+        # self.patterns = list()
 
     def padding_words(self, word):
         """
         :param word: 读入的单词
         :return: 根据单词匹配正则表达式，如果匹配成功则将其替换为pad
         """
-        pattern = re.compile(r'(\d|-|．|／|∶)+')
+        # 19980112-09-001-001
+        # 字母串
+        pattern_index = re.compile(r'((\d|-|∶|．|／|[０-９]|·|[ａ-ｚ]|[Ａ-Ｚ]|—)+)')
+        # 数字
+        pattern_numrate = re.compile(r'((百分之|第)?([０-９]+|[0-9]+|[○零一二三四五六七八九十]+)([百千万亿]?)[.．·点]?([０-９]+|[0-9]+|['
+                                     r'○零一二三四五六七八九十]+)([百千万亿]?)([年万千亿个％‰])*)*')
+        # pattern_sentence = re.compile(r'')
 
-        m = pattern.match(word)
-        if m is not None:
+        # 如果加入数组中遍历太慢了，只能这样
+        m1 = pattern_index.match(word)
+        m2 = pattern_numrate.match(word)
+        if m1 is not None:
             # print(m.group(0))
             # print(m.span(0))
-            index = m.span(0)
-            if index[1] != len(word):
-                self.s.add(word[index[1]])
-            print(word)
-            word = self.pad
+            index1 = m1.span()
+            # print(index)
+            # 完全匹配
+            if index1[1] == len(word):
+                print(word)
+                # 将word替换为pad
+                word = self.pad
+        if m2 is not None:
+            # print(m.group(0))
+            # print(m.span(0))
+            index2 = m2.span()
+            # print(index)
+            # 完全匹配
+            if index2[1] == len(word):
+                print(word)
+                # 将word替换为pad
+                word = self.pad
         return word
 
     def make_vocab(self):
@@ -63,7 +85,7 @@ class Vocab:
             words = get_sorted_list(self.d)
             for word in words:
                 f.write(word[0])
-                f.write(' ')
+                f.write('\t')
                 f.write(str(word[1]))
                 if word is not words[-1]:
                     f.write('\n')
@@ -78,11 +100,12 @@ class Vocab:
 
 def get_sorted_list(dict_in, reverse=False):
     """
+    按词的大小排序
     :param dict_in: 词典
     :param reverse: False 为升序，True为降序
     :return: 排序好的列表
     """
-    return sorted(dict_in.items(), key=lambda x: x[1], reverse=reverse)
+    return sorted(dict_in.items(), key=lambda x: len(x[0]), reverse=reverse)
 
 
 if __name__ == '__main__':
